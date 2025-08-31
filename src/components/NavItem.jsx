5bc88a7c0e34eef215d8972a81ddd7c9c6492768
pataha <pataha@gmx.de>
2025-08-31 12:52:14 +0200

    Add Friend.jsx

---
+++ src/components/NavItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavItem = ({itemTitle}) => {
+    return (
+        <li className="nav-item btn btn-danger mx-1">{itemTitle}</li>
+    );
+};
+
+export default NavItem;
