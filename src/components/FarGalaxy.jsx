ed4e762005829e8c4aad3b6c65a3e97baa714556
pataha <pataha@gmx.de>
2025-08-31 11:46:28 +0200

    First commit

---
+++ src/components/FarGalaxy.jsx
@@ -0,0 +1,11 @@
+import {openingCrawl} from "../utils/constants.js";
+
+const FarGalaxy = () => {
+    return (
+        <p className="farGalaxy">
+                {openingCrawl}
+        </p>
+    );
+};
+
+export default FarGalaxy;
